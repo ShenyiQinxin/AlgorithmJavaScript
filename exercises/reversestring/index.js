19beea72fcb0fcc725887015593324e7406ac501
macolulu <shenyiqinxin@gmail.com>
2018-02-25 13:54:42 -0500

    Adding reversestring/

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+
+
+
+module.exports = reverse;
+
+
+/*solution 1 : turn str into an array, call reverse method on the array, join the array back into a string, return result*/
+/*function reverse(str) {
+	
+}*/
+
+
+/*solution2: create an empty string called 'reversed',
+for each character in the string, take the character and add it to the start of reversed, return the variable 'reversed'*/
+function reverse(str) {
+	
+}
+
+/*solution3: reduce helper*/
+/*function reverse(str) {
+	
+}*/
+
+/*manually call the function that being debugered*/
+reverse('asdf');
+
+/*
+debugger steps
+add debugger stat in the func
+call the func manually after the func
+terminal, node inspect index.js
+to continue execution, c
+launch a repl session, repl
+exit the repl, Control+c
+exit all, exit
+
+*/
