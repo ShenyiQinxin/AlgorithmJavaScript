f8326a6fc553cc2e753f0610530b44c7049aa008
macolulu <shenyiqinxin@gmail.com>
2018-02-25 15:52:14 -0500

    palindrome added

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -14,20 +14,25 @@ module.exports = reverse;
 
 /*solution 1 : turn str into an array, call reverse method on the array, join the array back into a string, return result*/
 /*function reverse(str) {
-	
+	return str.split('').reverse().join('');
 }*/
 
 
 /*solution2: create an empty string called 'reversed',
 for each character in the string, take the character and add it to the start of reversed, return the variable 'reversed'*/
-function reverse(str) {
-	
-}
-
-/*solution3: reduce helper*/
 /*function reverse(str) {
-	
+	let reversed = '';
+	for(let character of str){
+		reversed = character + reversed;
+	}
+	return reversed;
 }*/
+
+/*solution3: reduce helper
+reduce(call-back, initial value)*/
+function reverse(str) {
+	return str.split('').reduce((rev, char) => char+rev, '');
+}
 
 /*manually call the function that being debugered*/
 reverse('asdf');
